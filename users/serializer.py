6703a2776b94e6dc019e0b6e1bf1f44413300be9
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-08-14 21:03:01 +0300

    3 users

--- users/serializer.py
+++ users/serializer.py
@@ -3,7 +3,14 @@ from rest_framework.serializers import ModelSerializer
 from users.models import User
 
 
+class PaymentsSerializer(ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = "__all__"
+
+
 class UserSerializer(ModelSerializer):
+    payments = PaymentsSerializer(source="payments_set", many=True, read_only=True)
     class Meta:
         model = User
         fields = "__all__"
